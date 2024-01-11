748f85c12e316f4969a7484479e1c99a0d6404a0
hsolkim615 <144770909+hsolkim615@users.noreply.github.com>
2024-01-11 18:04:28 +0900

    230111 KHS - 넥서스 및 억제기 생성 등
    230111 KHS - 넥서스 및 억제기 생성 등

---
+++ Source/InAbyss/Public/BD_Inhibitor.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Building_Base.h"
+#include "BD_Inhibitor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INABYSS_API ABD_Inhibitor : public ABuilding_Base
+{
+	GENERATED_BODY()
+	
+public:
+	void Resurrection();
+
+
+};
